4def4fc392c991e9c62341eeeb1f9bb01e3168c1
JD Harrington <jd@jdharrington.net>
2012-09-28 17:54:56 -0400

    move some stuff into spec_helper

--- spec/offender_spec.rb
+++ spec/offender_spec.rb
@@ -1,5 +1,5 @@
+require File.dirname(__FILE__) + '/spec_helper'
 require File.dirname(__FILE__) + '/../doorman/offender'
-require 'minitest/autorun'
 
 describe Offender do
   before do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+
+#require "mock_redis"
+
+#class ConnectableMockRedis < MockRedis
+#  def self.connect(attrs = {})
+#    MockRedis.new
+#  end
+#end
+
+#Redis = ConnectableMockRedis
